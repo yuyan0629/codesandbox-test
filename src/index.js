3396bf27ce271b116c65f6ede66bdcd1e2599ef8
Yuya Suzuki <yuyan0629@gmail.com>
2022-08-29 00:29:29 +0000

    Update 5 files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
-import "./styles.css";
+/** const let */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+var val1 = "var変数";
+console.log(val1);
